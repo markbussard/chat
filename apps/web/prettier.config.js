0139328dfb059ce294d62332906acd3a89023331
Mark Bussard <markbussard@outlook.com>
2025-02-08 19:09:11 -0800

    Install @repo/prettier-config in web and eslint-config

---
+++ apps/web/prettier.config.js
@@ -0,0 +1,12 @@
+import basePrettierConfig from "@repo/prettier-config";
+
+/** @typedef  {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const prettierConfig = {
+  ...basePrettierConfig
+}
+
+export default prettierConfig
