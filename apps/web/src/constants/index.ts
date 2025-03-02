6b7d15d50a5d8f202660730dbadfadc3e5df63db
Mark Bussard <markbussard@outlook.com>
2025-03-01 23:29:02 -0800

    Add META_THEME_COLORS to constants

--- apps/web/src/constants/index.ts
+++ apps/web/src/constants/index.ts
@@ -1,3 +1,8 @@
 export const SIDEBAR_COOKIE_NAME = "sidebar_state";
 
 export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
+
+export const META_THEME_COLORS = {
+  LIGHT: "#ffffff",
+  DARK: "#09090b"
+} as const;
