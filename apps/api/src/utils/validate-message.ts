a035a88620b13cdff199d9e3342c0f172fe01443
Mark Bussard <markbussard@outlook.com>
2025-03-12 08:49:08 -0700

    Update api message handler to be fully functional - Allow for continuing conversations with message history - Initialize user and assistant messages using generated ids

--- apps/api/src/utils/validate-message.ts
+++ apps/api/src/utils/validate-message.ts
@@ -1,11 +1,14 @@
 import z from "zod";
 
+import { MessageSender } from "@repo/db";
+
 import { safeParseMessageJSON } from "./messages";
 
 const WebSocketMessageSchema = z.object({
   chatId: z.string().cuid(),
-  messageId: z.string().cuid().optional(),
-  content: z.string().min(1).max(5000)
+  message: z.string().min(1).max(5000),
+  messageId: z.string().uuid(),
+  history: z.array(z.tuple([z.nativeEnum(MessageSender), z.string()]))
 });
 
 export function validateMessage(message: string) {
